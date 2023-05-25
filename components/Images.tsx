b7b68322f98e749257d60477889086a237a7698e
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-05-25 12:45:32 +0100

    add refresh button and spinner for when images are loading

--- components/Images.tsx
+++ components/Images.tsx
@@ -1,8 +1,12 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import useSWR from "swr";
 import Image from "next/image";
 import fetchImages from "@/lib/fetchImages";
+
+import { TiTick } from "react-icons/ti";
+import { MdOutlineContentCopy } from "react-icons/md";
+import MoonLoader  from "react-spinners/MoonLoader";
 
 type ImagesType = {
   name: string;
@@ -10,6 +14,9 @@ type ImagesType = {
 };
 
 const Images: FC = ({}) => {
+
+  const [copy, setCopy] = useState("");
+
   const {
     data: images,
     isLoading,
@@ -19,19 +26,42 @@ const Images: FC = ({}) => {
     revalidateOnFocus: false,
   });
 
+  const handleCopy = (copyUrl: string) => {
+    setCopy(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopy(""), 2000)
+  }
+
   return (
     <>
-      <div className="mt-10 mx-auto w-5/6 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+
+        <button className="fixed hover:hover:underline bg-dark-col-600 bottom-10 right-10 p-3 z-20 rounded-md shadow-2xl drop-shadow-xl" onClick={() => refreshImages(images)}>
+            {!isLoading && isValidating ? "Refreshing..." : "Refresh Images"}
+        </button>
+
+        {isLoading && (
+            <MoonLoader 
+            className="mx-auto mt-10"
+            color={"gray"}
+            size={150}
+            aria-label="Loading Spinner"/>
+        )}
+
+      <div className="my-14 mx-auto w-5/6 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {images?.imageURLs.map((image: ImagesType, i: number) => (
           <div key={image.name}
-          className={`relative cursor-help ${i === 0 && "md:col-span-2 md:row-span-2"}`}
+          className={`relative ${i === 0 && "md:col-span-2 md:row-span-2"}`}
           >
             {/* create div that shows only when user hovers over image */}
-            <div className="p-3 absolute flex z-10 justify-center items-center w-full h-full bg-dark-col-700 opacity-0 hover:opacity-80 transition-opacity duration-200">
+            <div className="p-3 absolute flex flex-col z-10 justify-center items-center w-full h-full bg-dark-col-700 opacity-0 hover:opacity-80 transition-opacity duration-200">
                 <p className="text-center text-md text-gray-300">
                     {/* remove timestamp and file extension from image name */}
                     {image.name.split("_").shift()?.toString().split(".").shift()}
                 </p>
+                <div className="mt-3 cursor-pointer hover:scale-125 transition duration-200" onClick={() => handleCopy(image.url)}>
+                    {copy === image.url ? <TiTick /> : <MdOutlineContentCopy />}
+                </div>
+                
             </div>
             <Image
               src={image.url}
