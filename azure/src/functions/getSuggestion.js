ffede35473e2f3c21760282fbc9e2bf965c65dae
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-05-25 12:39:40 +0100

    edit dall-e prompt

--- azure/src/functions/getSuggestion.js
+++ azure/src/functions/getSuggestion.js
@@ -8,7 +8,7 @@ app.http("getSuggestion", {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt:
-        "Create a random prompt for DALL-E to generate a random image that will be shown to the user. Include a number of details such as the genre, the type of painting, with options such as: watercolor, 4k, photo-realistic, modern, abstract, oil painting and more options of your choice. Do not wrap the answer in quotes.",
+        "Create a random prompt for DALL-E to generate a random image that will be shown to the user. Include a number of details such as the genre, the type of painting, with additional options such as: watercolor, 4k, black and white, photo-realistic, modern, abstract, oil painting, synthwave, renaissance, 3d, high definition, futuristic and more options of your choice. Do not wrap the answer in quotes.",
       max_tokens: 100,
       temperature: 0.8,
     });
